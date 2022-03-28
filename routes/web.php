4c160396b45ad76cba1d56cc42a5ce39b5473498
AlessandroImpact <terracciano41@gmail.com>
2022-03-28 20:01:42 +0200

    jumbotron e comics section

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('characters');
+    $comics = config('comics');
+    $infos = config('infos');
+    
+
+    return view('characters', ['data'=>$comics, 'infos'=>$infos]);
 });
 
 Route::get('/comics', function () {
